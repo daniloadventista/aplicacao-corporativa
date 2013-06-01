fd78ae68ed65f457bc05c566694b7ec9b9172611
THIAGO <THIAGO@THIAGO-PC>
2013-06-01 20:14:28 -0300

    CRUD - CLIENTE

--- Enterprise Application/Enterprise Application-war/src/java/br/com/projii/controller/UsuarioBean.java
+++ Enterprise Application/Enterprise Application-war/src/java/br/com/projii/controller/UsuarioBean.java
@@ -6,11 +6,16 @@ package br.com.projii.controller;
 
 import br.com.projii.jpa.Usuario;
 import br.com.projii.jpa.facade.UsuarioFacadeRemote;
+import com.sun.org.apache.bcel.internal.generic.BREAKPOINT;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
@@ -38,13 +43,14 @@ public class UsuarioBean {
         }
 
     }
-    private Usuario usuario;
+    private Usuario usuario = new Usuario(null, null);
     private int numero;
     @EJB
     private UsuarioFacadeRemote usuarioFacade;
     private Long id;
     private String nome;
     private String senha;
+    private String senha2;
     private char sexo;
     private String email;
     private String telefone;
@@ -146,26 +152,41 @@ public class UsuarioBean {
         this.isFunc = isFunc;
     }
 
+    public String getSenha2() {
+        return senha2;
+    }
+
+    public void setSenha2(String senha2) {
+        this.senha2 = senha2;
+    }
+
     public void create() {
-        usuario = new Usuario(this.getNome(), this.getSenha());
+        setUsuario(new Usuario(this.getNome(), this.getSenha()));
         //setar atributos do usuario
 
-        usuario.setCPF(this.getCPF());
-        usuario.setDataNasc(this.getDataNasc());
-        usuario.setEmail(this.getEmail());
-        usuario.setIsFunc(this.isIsFunc());
-        usuario.setNome(this.getNome());
-        usuario.setRG(this.getRG());
-        usuario.setSenha(this.getSenha());
-        usuario.setSexo(this.getSexo());
-        usuario.setTelefone(this.getTelefone());
+        getUsuario().setCPF(this.getCPF());
+        getUsuario().setDataNasc(this.getDataNasc());
+        getUsuario().setEmail(this.getEmail());
+        getUsuario().setIsFunc(this.isIsFunc());
+        getUsuario().setNome(this.getNome());
+        getUsuario().setRG(this.getRG());
+        getUsuario().setSenha(this.getSenha());
+        getUsuario().setSexo(this.getSexo());
+        getUsuario().setTelefone(this.getTelefone());
 
         try {
-            usuarioFacade.create(usuario);
-            System.out.println("Usuario Gravado.");
+            if (!usuario.getSenha().equals(getSenha2())) {
+                this.loginMessage = "Senhas não correspondem.";
+                System.out.println("Senhas não correspondem.");
+            } else {
+                usuarioFacade.create(getUsuario());
+                System.out.println("Usuario Gravado.");
+                this.loginMessage = "Usuálio Gravado";
+            }
         } catch (Exception e) {
             System.out.println();
             System.out.println("Erro ao gravar Usuario.");
+            this.loginMessage = "Erro ao gravar Usuario";
             System.out.println(e.toString());
             System.out.println();
         }
@@ -187,10 +208,10 @@ public class UsuarioBean {
         return usuarioFacade.find(id);
     }
 
-    public void login(){
+    public String login() throws IOException {
         List<Usuario> usuarios = null;
         usuarios = this.findAll();
-        
+
         boolean existe = false;
         for (Usuario usr : usuarios) {
             if (this.email.equals(usr.getEmail())) {
@@ -198,8 +219,11 @@ public class UsuarioBean {
                 if (senha.equals(usr.getSenha())) {
                     System.out.println("Usuálio Logado");
                     this.loginMessage = "Usuálio Logado";
-                    break;
+                  //  callFind(usr.getId());
+                   // FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("nome", nome);
+                    FacesContext.getCurrentInstance().getExternalContext().redirect("/Enterprise_Application-war/minhaConta.xhtml");
                 } else {
+                    //FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
                     System.out.println("Senha Invalida");
                     this.loginMessage = ("Senha Invalida");
                 }
@@ -209,6 +233,75 @@ public class UsuarioBean {
             System.out.println("Usuario Inexistente");
             this.loginMessage = ("Usuario Inexistente");
         }
+        return "";
+    }
 
+//    public void callFind(long id) {
+//        try {
+//
+//            this.setUsuario(find(id));
+//            this.nome = getUsuario().getNome();
+//            this.email = getUsuario().getEmail();
+//            this.senha = getUsuario().getSenha();
+//            this.telefone = getUsuario().getTelefone();
+//            this.CPF = getUsuario().getCPF();
+//            this.RG = getUsuario().getRG();
+//            this.dataNasc = getUsuario().getDataNasc();
+//            this.sexo = getUsuario().getSexo();
+//
+//        } catch (Exception e) {
+//            System.out.print("buscar");
+//        }
+//    }
+
+       public String alterarCliente(){
+
+        UsuarioBean user =  new UsuarioBean();
+        user.update(usuario);
+        return "minhaConta";
+
+    }
+    private DataModel listaDados;
+
+    public DataModel getListarClientes() {
+//        List<Usuario> usuarios = null;
+//        usuarios = this.findAll();
+//        for (Usuario usr : usuarios) {
+//
+//            if (this.email.equals(usr.getEmail()) && senha.equals(usr.getSenha())) {
+                List<Usuario> lista = new UsuarioBean().findAll();
+                listaDados = new ListDataModel(lista);
+//                
+//            }
+//        }
+                return listaDados;
+
+    }
+    public String prepararAlterarCliente(){
+        setUsuario((Usuario)(listaDados.getRowData()));
+        return "minhaConta";
+    }
+    
+    public String excluirCliente(){
+
+        Usuario cad = (Usuario)(listaDados.getRowData());
+        UsuarioBean user = new UsuarioBean();
+        user.delete(cad);
+        return "minhaConta";
+
+    }
+
+    /**
+     * @return the usuario
+     */
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 }
